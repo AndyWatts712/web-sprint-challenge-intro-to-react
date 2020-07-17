578ba347928f8d67f0d1a9bb8dab41344b1f79a2
megawatts68 <andrew.f.watts@gmail.com>
2020-07-18 00:08:51 +0700

    Working, needs styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,28 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import Character from './components/Character'
+import Character from './components/Character';
+import Styled from 'styled-components';
 import './App.css';
 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [characters, setCharacters] = useState()
+const [data, setData] = useState()
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
     axios.get('https://swapi.dev/api/people/')
       .then(resp => {
-        setCharacters(resp.data.results)
+        setData(resp.data.results)
+        console.log('DATA', resp.data.results)
       })
       .catch(err => console.log(err))
   }, [])
-console.log(characters)
+console.log('CHARACTERS', data)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character characters = {characters} />
+      {data && <Character list = {data} />}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,8 +1,29 @@
 // Write your Character component here
 import React from 'react';
+import Styled from 'styled-components';
 
-function Character() {
-    return null
+
+function Character(props) {
+    console.log('PROPS', props)
+    const { list } = props
+    // console.log(list)
+
+    // console.log(chr)
+    return (
+        <>
+            {list.map(chr => {
+                return (
+                    <div>
+                        <h2>{chr.name}</h2>
+                        <p>Height: {chr.height}</p>
+                        <p>Mass: {chr.mass}</p>
+                        {/* <p>{chr.}</p> */}
+                        {/* <p>{chr.}</p> */}
+                    </div>
+                )
+            })}
+        </>
+    )
 }
 
 export default Character
